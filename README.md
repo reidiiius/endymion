0fcd3e8cfe02c2b0ae338cfa501f491eded70991
Reid Netterville III <reidiiius@gmail.com>
2016-09-16 19:17:45 -0500

    160916.0

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# endymion
+# Endymion
 Selenography
+
+### Usage
+command line:
+
+    git clone https://github.com/reidiiius/endymion.git
+
+    cd endymion && lua chromia.lua
+
+### Distribution
+MIT License
+
---
+++ chromia.lua
@@ -0,0 +1,116 @@
+#!/usr/bin/env lua
+
+PROTOGENEIA = {}
+
+PROTOGENEIA.Eurycyda = function()
+
+  local axius = {}
+      axius["j2"]="HgHg PuFe ____ ____ CuNp PbAu ____ AuPb ____ AgUr ____ FePu "
+      axius["j3"]="HgSn ____ SnHg UrFe ____ PbAg ____ AuAu ____ AgPb ____ FeUr "
+      axius["j5"]="PbCu ____ AuSn ____ AgHg TiFe FeTi ____ ____ SnAu ____ CuPb "
+      axius["j6"]="HgAu ____ SnPb ____ CuUr PbSn ____ AuHg NpFe ____ ____ FeNp "
+      axius["k1"]="____ FeUr HgSn ____ SnHg UrFe ____ PbAg ____ AuAu ____ AgPb "
+      axius["k2"]="NpCu ____ ____ FePu HgHg PuFe ____ UrAg ____ PbAu ____ AuPb "
+      axius["k5"]="UrCu ____ PbSn ____ AuHg NpFe ____ ____ FeNp HgAu ____ SnPb "
+      axius["k6"]="HgAg ____ SnAu ____ CuPb PbCu ____ AuSn ____ ____ TiFe FeTi "
+      axius["n0"]="HgCu ____ SnSn ____ CuHg PbFe ____ AuAg ____ AgAu ____ FePb "
+     axius["j17"]="____ ____ SnAu ____ CuPb PbCu ____ AuSn ____ AgHg TiFe FeTi "
+     axius["j23"]="HgHg PuFe ____ UrAg ____ PbAu ____ AuPb ____ AgUr ____ FePu "
+     axius["j25"]="FeCu HgMn ____ ____ MnHg CuFe PbTi ____ ____ NpAu ____ TiPb "
+     axius["j26"]="HgHg PuFe ____ ____ CuNp PbAu ____ AuPb NpCu ____ ____ FePu "
+     axius["j36"]="HgAu ____ SnPb UrCu ____ PbSn ____ AuHg NpFe ____ ____ FeNp "
+     axius["j56"]="UrCu ____ PbSn ____ AuHg NpFe AgTi ____ FeNp ____ ____ SnPb "
+     axius["k12"]="____ AgUr ____ FePu HgHg PuFe ____ UrAg ____ PbAu ____ AuPb "
+     axius["k15"]="____ CuUr PbSn ____ AuHg NpFe ____ ____ FeNp HgAu ____ SnPb "
+     axius["k25"]="NpCu ____ ____ FePu HgHg PuFe ____ ____ CuNp PbAu ____ AuPb "
+     axius["k26"]="HgMn ____ ____ MnHg CuFe PbTi ____ AuNp ____ ____ TiPb FeCu "
+     axius["k34"]="PbCu ____ AuSn ____ ____ TiFe FeTi HgAg ____ SnAu ____ CuPb "
+     axius["k56"]="HgAu ____ SnPb ____ CuUr PbSn ____ ____ NpFe ____ TiAg FeNp "
+    axius["j236"]="HgHg PuFe ____ UrAg ____ PbAu ____ AuPb NpCu ____ ____ FePu "
+    axius["j256"]="FeCu HgMn ____ ____ MnHg CuFe PbTi ____ AuNp ____ ____ TiPb "
+    axius["j2k5"]="FeCu HgMn ____ ____ MnHg CuFe ____ ____ AuNp NpAu ____ TiPb "
+    axius["j2k6"]="HgHg PuFe ____ ____ CuNp PbAu ____ AuPb ____ ____ TiSn FePu "
+    axius["j2y3"]="HgHg PuFe SnTi ____ ____ PbAu ____ AuPb ____ AgUr ____ FePu "
+    axius["j3k5"]="NpCu ____ TiSn FePu ____ PuFe ____ ____ CuNp PbAu ____ AuPb "
+    axius["j3k6"]="HgTi ____ SnNp UrAu ____ PbPb ____ AuUr ____ ____ TiHg FeFe "
+    axius["j5y6"]="PbCu ____ AuSn ____ AgHg TiFe FeTi HgAg ____ ____ ____ CuPb "
+    axius["k125"]="____ AgUr ____ FePu HgHg PuFe ____ ____ CuNp PbAu ____ AuPb "
+    axius["k1j5"]="____ AuUr NpSn ____ TiHg FeFe HgTi ____ ____ UrAu ____ PbPb "
+    axius["k1j6"]="____ PuFe SnTi ____ CuNp PbAu ____ AuPb NpCu ____ ____ FePu "
+    axius["k256"]="HgMn ____ ____ MnHg CuFe PbTi ____ ____ NpAu ____ TiPb FeCu "
+    axius["k2j5"]="NpCu ____ ____ FePu HgHg PuFe SnTi ____ ____ PbAu ____ AuPb "
+    axius["k2j6"]="HgMn ____ ____ MnHg CuFe PbTi ____ AuNp NpAu ____ ____ FeCu "
+    axius["k2x1"]="____ ____ TiSn FePu HgHg PuFe ____ UrAg ____ PbAu ____ AuPb "
+    axius["k6x5"]="HgAg ____ SnAu ____ CuPb PbCu ____ ____ ____ AgHg TiFe FeTi "
+    axius["n167"]="NpCu ____ ____ FePu ____ PuFe SnTi ____ CuNp PbAu ____ AuPb "
+    axius["n345"]="____ PuFe ____ ____ CuNp PbAu ____ AuPb NpCu ____ TiSn FePu "
+    axius["n5y2"]="HgMn ____ ____ MnHg CuFe ____ ____ AuNp NpAu ____ TiPb FeCu "
+    axius["n6x2"]="FeCu HgMn ____ ____ MnHg CuFe PbTi ____ AuNp NpAu ____ ____ "
+   axius["j17k2"]="____ ____ ____ MnFe CuTi PbAg ____ AuAu ____ AgPb TiCu FeMn "
+   axius["j17y2"]="HgAg ____ ____ ____ CuPb PbCu ____ AuSn ____ AgHg TiFe FeTi "
+   axius["j23k6"]="HgHg PuFe ____ UrAg ____ PbAu ____ AuPb ____ ____ TiSn FePu "
+   axius["j25y6"]="TiCu FeMn ____ ____ SnHg MnFe CuTi PbAg ____ ____ ____ AgPb "
+   axius["j26y3"]="HgHg PuFe SnTi ____ ____ PbAu ____ AuPb NpCu ____ ____ FePu "
+   axius["j2k34"]="TiCu FeMn ____ ____ ____ MnFe CuTi PbAg ____ AuAu ____ AgPb "
+   axius["j2k56"]="HgHg PuFe ____ ____ CuNp PbAu ____ ____ NpCu ____ TiSn FePu "
+   axius["j34k6"]="HgSn ____ SnHg MnFe CuTi ____ ____ AuAu ____ ____ TiCu FeMn "
+   axius["j56y7"]="UrCu ____ PbSn ____ AuHg NpFe AgTi ____ FeNp HgAu ____ ____ "
+   axius["k12j5"]="____ AgUr ____ FePu HgHg PuFe SnTi ____ ____ PbAu ____ AuPb "
+   axius["k17j5"]="TiCu FeMn HgSn ____ SnHg MnFe CuTi ____ ____ AuAu ____ ____ "
+   axius["k25x1"]="____ ____ TiSn FePu HgHg PuFe ____ ____ CuNp PbAu ____ AuPb "
+   axius["k26x5"]="HgSn ____ ____ MnFe CuTi PbAg ____ ____ ____ AgPb TiCu FeMn "
+   axius["k2j56"]="NpCu ____ ____ FePu HgHg PuFe SnTi ____ CuNp ____ ____ AuPb "
+   axius["k34x2"]="PbCu ____ ____ ____ AgHg TiFe FeTi HgAg ____ SnAu ____ CuPb "
+   axius["k56x4"]="HgAu ____ SnPb ____ CuUr ____ ____ AuHg NpFe ____ TiAg FeNp "
+   axius["n25x6"]="TiCu FeMn HgSn ____ ____ MnFe CuTi PbAg ____ ____ ____ AgPb "
+   axius["n26y5"]="____ ____ SnHg MnFe CuTi PbAg ____ ____ ____ AgPb TiCu FeMn "
+   axius["n45y2"]="HgTi ____ ____ UrAu ____ PbPb ____ AuUr NpSn ____ TiHg FeFe "
+   axius["n67x2"]="____ AuUr ____ ____ TiHg FeFe HgTi ____ SnNp UrAu ____ PbPb "
+  axius["j136y7"]="____ ____ SnPb UrCu ____ PbSn ____ AuHg NpFe AgTi ____ FeNp "
+  axius["j167y2"]="HgAu ____ ____ ____ CuUr PbSn ____ AuHg NpFe ____ TiAg FeNp "
+  axius["j246y3"]="HgHg PuFe SnTi ____ CuNp ____ ____ AuPb NpCu ____ ____ FePu "
+  axius["j26y34"]="HgHg PuFe SnTi UrAg ____ ____ ____ AuPb NpCu ____ ____ FePu "
+  axius["j2k6x5"]="HgHg PuFe ____ ____ CuNp PbAu ____ ____ ____ AgUr TiSn FePu "
+  axius["j2k6y3"]="HgHg PuFe SnTi ____ ____ PbAu ____ AuPb ____ ____ TiSn FePu "
+  axius["j346y5"]="NpCu ____ TiSn FePu HgHg PuFe ____ ____ CuNp ____ ____ AuPb "
+  axius["j3k5x4"]="HgAu ____ SnPb UrCu ____ ____ ____ AuHg NpFe AgTi ____ FeNp "
+  axius["k135x4"]="____ CuUr PbSn ____ ____ NpFe ____ TiAg FeNp HgAu ____ SnPb "
+  axius["k157x6"]="HgHg PuFe SnTi ____ CuNp PbAu ____ ____ NpCu ____ ____ FePu "
+  axius["k1j6y7"]="____ CuUr PbSn ____ AuHg NpFe ____ TiAg FeNp HgAu ____ ____ "
+  axius["k257x1"]="NpCu ____ TiSn FePu HgHg PuFe ____ ____ CuNp PbAu ____ ____ "
+  axius["k25x17"]="____ AgUr TiSn FePu HgHg PuFe ____ ____ CuNp PbAu ____ ____ "
+  axius["k2j5x1"]="____ ____ TiSn FePu HgHg PuFe SnTi ____ ____ PbAu ____ AuPb "
+  axius["k2j5y6"]="NpCu ____ ____ FePu HgHg PuFe SnTi UrAg ____ ____ ____ AuPb "
+  axius["k345x2"]="UrCu ____ ____ ____ AuHg NpFe AgTi ____ FeNp HgAu ____ SnPb "
+  axius["n167x4"]="HgAu ____ ____ UrCu ____ PbSn ____ AuHg NpFe AgTi ____ FeNp "
+  axius["n345y7"]="____ CuUr ____ ____ AuHg NpFe ____ TiAg FeNp HgAu ____ SnPb "
+ axius["j2k56x4"]="HgHg PuFe ____ ____ CuNp ____ ____ AuPb NpCu ____ TiSn FePu "
+ axius["j3k56x4"]="HgTi ____ SnNp UrAu ____ ____ ____ AuUr NpSn ____ TiHg FeFe "
+ axius["k1j56y7"]="____ AuUr NpSn ____ TiHg FeFe HgTi ____ SnNp UrAu ____ ____ "
+ axius["k2j56y7"]="NpCu ____ ____ FePu HgHg PuFe SnTi ____ CuNp PbAu ____ ____ "
+
+  local aetolus = os.time()
+
+  print()
+
+  for paeon, epeius in pairs(axius) do
+    Fn, Cn, Gn, Dn, An, En, Bn =
+    25,  0, 35, 10, 45, 20, 55
+    print()
+    print(paeon .. '-' .. aetolus)
+    print(string.sub(epeius, (Fn + 1), -1) .. string.sub(epeius, Cn, (Fn - 1)))
+    print(string.sub(epeius, (Cn + 1), -1) .. string.sub(epeius, Cn, (Cn - 0)))
+    print(string.sub(epeius, (Gn + 1), -1) .. string.sub(epeius, Cn, (Gn - 1)))
+    print(string.sub(epeius, (Dn + 1), -1) .. string.sub(epeius, Cn, (Dn - 1)))
+    print(string.sub(epeius, (An + 1), -1) .. string.sub(epeius, Cn, (An - 1)))
+    print(string.sub(epeius, (En + 1), -1) .. string.sub(epeius, Cn, (En - 1)))
+    print(string.sub(epeius, (Bn + 1), -1) .. string.sub(epeius, Cn, (Bn - 1)))
+    print()
+  end
+
+  print()
+
+end
+
+PROTOGENEIA.Eurycyda()
+
