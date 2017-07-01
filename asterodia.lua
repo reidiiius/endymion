65cb1675531e55437d040bb73b15873c708f8ce8
Reid Netterville III <reidiiius@gmail.com>
2017-07-01 02:42:02 -0500

    170701.0

--- asterodia.lua
+++ asterodia.lua
@@ -13,15 +13,26 @@ PROTOGENEIA.Eurycyda = function()
     local Fn, Cn, Gn, Dn, An, En, Bn =
           25,  0, 35, 10, 45, 20, 55
     -- instrument tuning
+
+    local s0, s1, s2, s3, s4, s5, s6, s7 =
+    paeon .. '-beadgcf-v' .. aetolus,
+    string.sub(epeius, (Fn + 1), -1) .. string.sub(epeius, Cn, Fn),
+    string.sub(epeius, (Cn + 1), -1) .. string.sub(epeius, Cn, Cn),
+    string.sub(epeius, (Gn + 1), -1) .. string.sub(epeius, Cn, Gn),
+    string.sub(epeius, (Dn + 1), -1) .. string.sub(epeius, Cn, Dn),
+    string.sub(epeius, (An + 1), -1) .. string.sub(epeius, Cn, An),
+    string.sub(epeius, (En + 1), -1) .. string.sub(epeius, Cn, En),
+    string.sub(epeius, (Bn + 1), -1) .. string.sub(epeius, Cn, Bn)
+
     print()
-    print(paeon .. '-v' .. aetolus)
-    print(string.sub(epeius, (Fn + 1), -1) .. string.sub(epeius, Cn, Fn))
-    print(string.sub(epeius, (Cn + 1), -1) .. string.sub(epeius, Cn, Cn))
-    print(string.sub(epeius, (Gn + 1), -1) .. string.sub(epeius, Cn, Gn))
-    print(string.sub(epeius, (Dn + 1), -1) .. string.sub(epeius, Cn, Dn))
-    print(string.sub(epeius, (An + 1), -1) .. string.sub(epeius, Cn, An))
-    print(string.sub(epeius, (En + 1), -1) .. string.sub(epeius, Cn, En))
-    print(string.sub(epeius, (Bn + 1), -1) .. string.sub(epeius, Cn, Bn))
+    print(string.format("\t%s", s0))
+    print(string.format("\t%s", s1))
+    print(string.format("\t%s", s2))
+    print(string.format("\t%s", s3))
+    print(string.format("\t%s", s4))
+    print(string.format("\t%s", s5))
+    print(string.format("\t%s", s6))
+    print(string.format("\t%s", s7))
     print()
 
   end
